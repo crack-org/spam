1b253dd21782c913efb89559853d3e073dd1ccaf
DarkHacker <65758782+crack-org@users.noreply.github.com>
2020-06-20 15:56:04 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Sedang meng install bahan..."
+echo " "
+pkg update && pkg upgrade
+pkg install figlet
+pkg install python && pkg install python2
+pip2 install request
+pkg install php
+echo "Install bahan selesai..."
